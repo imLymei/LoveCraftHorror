f1ccd4250b9c423328da138cef0b5e97b4ea0c76
imlymei <lymeicontato@gmail.com>
2026-09-10 21:13:51 -0300

    feat: add basic tilemap and screen displays

--- Scenes/Player/Player.gd
+++ Scenes/Player/Player.gd
@@ -5,7 +5,7 @@ const MOVE_SPEED: float = 64
 
 
 func _physics_process(_delta: float) -> void:
-	var input_direction := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	var input_direction := Input.get_vector("move_left", "move_right", "move_up", "move_down")
 	velocity = input_direction * MOVE_SPEED
 	
 	move_and_slide()
