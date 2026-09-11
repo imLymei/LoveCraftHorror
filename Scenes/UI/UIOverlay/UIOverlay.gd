e407ee7636e5bb0bb7059024747c8574ac2b7d80
joaovianaAlves <joaovitorvianaalves@gmail.com>
2026-09-10 23:14:27 -0300

    Fix: files order and names

--- Scenes/UI/UIOverlay/UIOverlay.gd
+++ Scenes/UI/UIOverlay/UIOverlay.gd
@@ -2,7 +2,7 @@ class_name UIOverlay
 extends CanvasLayer
 
 
-@onready var inventory_ui: Control = %InventoryUI
+@onready var inventory_UI: Control = %InventoryUI
 
 
 
@@ -16,11 +16,11 @@ func _ready() -> void:
 func open_inventory() -> void:
 	if current_menu:
 		current_menu.visible = false
-		if current_menu == inventory_ui:
+		if current_menu == inventory_UI:
 			current_menu = null
 			GameManager.player_can_walk = true
 			return
 	
-	current_menu = inventory_ui
+	current_menu = inventory_UI
 	current_menu.visible = true
 	GameManager.player_can_walk = false
