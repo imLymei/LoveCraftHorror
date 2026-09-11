773720465226904fb8fdb2c3c707e9773b7238b8
imlymei <lymeicontato@gmail.com>
2026-09-10 23:04:45 -0300

    refactor: fix bad file structure

--- Scenes/UI/UIOverlay/UIOverlay.gd
+++ Scenes/UI/UIOverlay/UIOverlay.gd
@@ -1,8 +1,9 @@
+class_name UIOverlay
 extends CanvasLayer
 
-class_name UIOverlay
 
-@onready var inventory_UI: Control = %InventoryUi
+@onready var inventory_ui: Control = %InventoryUI
+
 
 var current_menu: Control = null
 
@@ -15,11 +16,11 @@ func open_inventory() -> void:
 	print("open")
 	if current_menu:
 		current_menu.visible = false
-		if current_menu == inventory_UI:
+		if current_menu == inventory_ui:
 			current_menu = null
 			GameManager.player_can_walk = true
 			return
 	
-	current_menu = inventory_UI
+	current_menu = inventory_ui
 	current_menu.visible = true
 	GameManager.player_can_walk = false
